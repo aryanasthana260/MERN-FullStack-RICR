11cfe816633f24be45b929a79410e9e624f5eedc
aryanasthana260 <aryanasthana260@gmail.com>
2025-11-26 18:14:33 +0530

    calculator done

--- Javascript/Calculator Assignment/calculator.js
+++ Javascript/Calculator Assignment/calculator.js
@@ -1,21 +1,17 @@
-function appendValue(value) {
-  const display = document.getElementById("display");
-  display.value = display.value + value;
-}
-
-function clearDisplay() {
-  const display = document.getElementById("display");
-  display.value = "";
-}
-
-function calculate() {
-  const display = document.getElementById("display");
-
-  try {
-    // For a simple practice calculator
-    const result = eval(display.value);
-    display.value = result;
-  } catch (error) {
-    alert("Invalid expression");
+function Input(char) {
+  if (char === "=") {
+    try {
+      document.getElementById("display").value = eval(
+        document.getElementById("display").value
+      );
+    } catch (error) {
+      alert("Invalid Expression");
+      document.getElementById("display").value = "";
+    }
+  } else if (char === "C") {
+    document.getElementById("display").value = "";
+  } else {
+    document.getElementById("display").value =
+      document.getElementById("display").value + char;
   }
 }
