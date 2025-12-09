ba7ae4cae2415c858231f3cd15d30aa1d2c97734
aryanasthana260 <aryanasthana260@gmail.com>
2025-12-09 20:12:50 +0530

    Test Submitted

---
+++ Test/script.js
@@ -0,0 +1,10 @@
+function ContactInfo()
+{
+    const nm = document.getElementById("name").value;
+    const em = document.getElementById("email").value;
+    const me = document.getElementById("message").value;
+
+    console.log("Name :"+nm);
+    console.log("Email :"+em);
+    console.log("Message :"+me);
+}
