37ea0b22ddf376ba1cb0b159ca55f3f2baa43af8
aryanasthana260 <aryanasthana260@gmail.com>
2025-12-26 20:48:55 +0530

    did some react and made a simple webpage learned paths/routes

--- REACT/Demo/src/App.jsx
+++ REACT/Demo/src/App.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import "bootstrap/dist/css//bootstrap.min.css"; //for css
+import "bootstrap/dist/js/bootstrap.min.js"; //for js
+import "bootstrap-icons/font/bootstrap-icons.css"; // for icons
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Product from "./pages/Product";
+
 function App() {
-  let a = 5;
-  let b = -10;
-
-  console.log(a + b);
-
   return (
     <>
-      <h1 id="head">Hello World</h1>
-      <div>This is my First React App</div>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
-
 
 export default App;
---
+++ REACT/Demo/src/pages/Home.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="bg-primary text-lightnpm ">
+        {" "}
+        This is my First React App
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quaerat
+        asperiores saepe iste repellendus dolore perferendis maxime. Porro in
+        animi distinctio quisquam tenetur quod, quos earum asperiores velit sed
+        fugit repellendus laborum dicta. Ea excepturi obcaecati in error eius
+        distinctio, ipsa cupiditate rerum totam facere aut necessitatibus nisi
+        mollitia animi, voluptate, pariatur quia commodi qui consequuntur quod
+        ab! Saepe pariatur, rem, natus quod recusandae soluta non dolorum vel
+        possimus cumque, itaque dolores illum aperiam! Iusto cupiditate labore
+        vitae hic placeat inventore. Eos ab at fugiat recusandae natus? Ullam
+        nemo perspiciatis sunt aliquid blanditiis, laudantium voluptates beatae
+        fugit quod voluptate alias sit dolores, explicabo modi. Quia reiciendis
+        accusantium nemo amet in quo, esse omnis nobis quod. Temporibus
+        laboriosam neque id facere eos voluptatem quibusdam sit perspiciatis
+        soluta doloremque, ad, fuga aspernatur voluptates totam voluptate at
+        repellat! Necessitatibus ea sapiente architecto sequi perferendis dolore
+        veritatis aliquid. Molestias fugit, quas voluptates doloribus ipsum
+        tempora sit ullam fugiat asperiores recusandae officiis deleniti odit,
+        officia sed repellendus ducimus, doloremque consequuntur distinctio
+        esse? Sed, possimus odit ipsum distinctio neque vero culpa incidunt cum
+        in natus doloribus quas quasi reiciendis commodi suscipit, cupiditate
+        doloremque corporis minima ipsam et quae delectus! Natus ipsa dolores
+        earum doloremque quo rerum recusandae optio eaque labore veritatis unde
+        quas laboriosam veniam nihil cupiditate, provident fugit perspiciatis
+        ipsam? Illo eligendi veniam expedita tenetur ut facilis sapiente totam
+        et commodi tempore adipisci ea, modi itaque. Hic, repudiandae eos nam
+        repellendus tempora libero inventore eveniet, saepe alias facilis
+        similique iste est deleniti, nostrum suscipit necessitatibus doloribus
+        quidem at. Modi deserunt totam quis iste, sunt ex consectetur laborum.
+        Porro consequatur distinctio repellat amet aliquid, perspiciatis vel
+        consectetur, labore sequi sapiente facere nesciunt officiis voluptates?
+        Natus illo dolores dolorem et incidunt sit quam eos? Delectus error vero
+        repudiandae veniam voluptate quibusdam ipsam, omnis facere nisi impedit
+        magnam maxime blanditiis explicabo exercitationem a repellat est
+        reiciendis recusandae quasi illum beatae. Aspernatur, atque ratione quam
+        dolorum, hic quo eaque magni saepe nesciunt ducimus similique suscipit
+        sunt. Tempora, libero vero cum, possimus laboriosam, aliquam iste
+        expedita repellendus repellat totam iure error. Voluptatibus porro
+        delectus culpa tempore aperiam repellendus odio corrupti totam dolore
+        voluptas! Ratione sapiente pariatur dicta quod, illo nobis iure
+        provident doloremque dolores quidem tempora nemo recusandae nisi magni.
+        Itaque, omnis. Eos necessitatibus mollitia minus, doloremque voluptate
+        itaque nihil cumque quibusdam voluptatem, similique quasi voluptates
+        illum a rem architecto provident exercitationem et accusantium ullam,
+        aliquam amet recusandae eum tempora consequuntur! Unde minima quaerat
+        officiis hic quasi. Mollitia aliquam quasi rerum corporis optio amet
+        repellat nisi eligendi ullam quam tenetur, voluptate obcaecati ad quis!
+        Mollitia facere, repudiandae corrupti ex voluptatum dolores perferendis
+        nisi temporibus cum aut delectus doloribus voluptates eaque molestiae
+        enim doloremque hic? Mollitia aperiam esse eaque ullam possimus ab
+        dolorum iusto ipsum qui cum iure incidunt saepe necessitatibus
+        consequuntur laborum porro exercitationem reiciendis libero, expedita
+        vel pariatur modi animi? Accusamus tempore tempora doloremque voluptatem
+        reprehenderit, expedita voluptas eos asperiores? Dolores, nobis earum,
+        facere maxime vitae quod minima veritatis laborum accusantium ipsum
+        molestias incidunt quaerat delectus dignissimos, maiores dicta!
+      </p>
+    </>
+  );
+};
+
+export default Home;
