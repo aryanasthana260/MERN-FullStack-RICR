2f0ffecccb608c383632be59f32f498df1cd52ae
aryanasthana260 <aryanasthana260@gmail.com>
2025-12-31 10:10:48 +0530

    lil changes in tipcalculator js added tip amt seperate and tip with bill

--- Javascript/TipCalculator/Tip.js
+++ Javascript/TipCalculator/Tip.js
@@ -1,25 +1,24 @@
-  document.getElementById('calculateBtn').addEventListener('click', function() {
-           
-            var billAmount = document.getElementById('billAmount').value;
-            var serviceQuality = document.getElementById('serviceQuality').value;
-            var numPeople = document.getElementById('numPeople').value;
-            
-            
-            if (billAmount == '' || numPeople == '') {
-                alert('Please fill all fields!');
-                return;
-            }
-            
-            // Calculate tip amount
-            var tipAmount = (billAmount * serviceQuality) / 100;
-            
-            // Calculate total amount with tip
-            var totalAmount = Number(billAmount) + Number(tipAmount);
-            
-            // Calculate amount per person
-            var amountPerPerson = totalAmount / numPeople;
-            
-           
-            document.getElementById('resultAmount').textContent = amountPerPerson.toFixed(2) + '$';
-            document.getElementById('resultSection').style.display = 'block';
-        });
+document.getElementById("calculateBtn").addEventListener("click", function () {
+  var billAmount = document.getElementById("billAmount").value;
+  var serviceQuality = document.getElementById("serviceQuality").value;
+  var numPeople = document.getElementById("numPeople").value;
+
+  if (billAmount == "" || numPeople == "") {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  // Calculate tip amount
+  var tipAmount = (billAmount * serviceQuality) / 100;
+
+  // Calculate total amount with tip
+  var totalAmount = Number(billAmount) + Number(tipAmount);
+
+  // Calculate amount per person
+  var amountPerPerson = totalAmount / numPeople;
+
+  document.getElementById("tipAmount").textContent = tipAmount.toFixed(2) + "$";
+  document.getElementById("resultAmount").textContent =
+    amountPerPerson.toFixed(2) + "$";
+  document.getElementById("resultSection").style.display = "block";
+});
