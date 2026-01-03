03e6011fa6e6da3ce13fe3811ccd9e7b44fdc5fd
aryanasthana260 <aryanasthana260@gmail.com>
2026-01-03 21:48:22 +0530

    made currency converter

--- REACT/CurrencyConverter/src/App.jsx
+++ REACT/CurrencyConverter/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Header from './components/Header';
-import Currency from './components/Currency';
+import React from "react";
+import Header from "./components/Header";
+import Currency from "./components/Currency";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <Currency/>
+      <Toaster />
+      <Header />
+      <Currency />
     </>
   );
-}
+};
 
-export default App
+export default App;
